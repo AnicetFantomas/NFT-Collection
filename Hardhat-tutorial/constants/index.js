509d0def99770111ecd70da4c3b98f4f2d28afbc
AnicetFantomas <anicetmurhula20@gmail.com>
2023-03-06 15:47:14 +0200

    Deploy contract and create client side

---
+++ Hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x956701562ee27b9d3b492B2183D9BC0Baf70C109";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
